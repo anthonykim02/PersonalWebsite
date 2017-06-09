7ee548276d8a20f0370da25d49eecd5f4b23ef1a
Anthony Kim <anthony@Anthonys-MBP.hsd1.ca.comcast.net>
2017-06-09 14:23:54 -0700

    finished besides project details

---
+++ scripts.js
@@ -0,0 +1,66 @@
+
+// function changeTitle() {
+
+// 	var num = 1;
+ 	
+//  	if (num == 1) {
+//  		$("#ability").html("S O F T W A R E &nbsp; E N G I N E E R");
+//  	} else if (num == 2) {
+//  		$("#ability").html("G A M E &nbsp; D E S I G N E R");
+//  	} else if (num == 3) {
+//  		$("#ability").html("M O B I L E / W E B &nbsp; D E V E L O P E R");
+//  	} else if (num == 4) {
+//  		$("#ability").html("I N N O V A T O R");
+//  	}
+
+//  	num++;
+//  	if (num == 5) {
+//  		num = 1;
+//  	}
+
+// }
+
+// var timer = setInterval(changeTitle, 2000);
+
+
+
+
+// function scrollUp(d){
+//     var start = self.pageYOffset;
+//     var goal = document.getElementById(d).offsetTop;
+//     var current = start;
+
+//     var timer = setInterval(function() {
+//     	current += 9;
+//     	var diff = current - goal
+//     	if (diff <= 9 && diff >= -9) {
+//     		clearInterval(timer);
+//     	}
+//     	window.scrollTo(0, current);
+//     }, 1);
+// }
+
+// document.getElementById("about").onclick=function() {
+// 	scrollUp("aboutdiv");
+// }
+
+// document.getElementById("sendemail").onclick=function() {
+// 	window.location.href = "mailto:anthonykim623@gmail.com";
+// }
+
+// document.getElementById("facebook").onclick=function() {
+// 	window.open("http://www.facebook.com/anthony.kim.3591");
+// }
+
+// document.getElementById("linkedin").onclick=function() {
+// 	window.open("http://www.linkedin.com/in/anthony-kim-02/");
+// }
+
+// document.getElementById("github").onclick=function() {
+// 	window.open("http://github.com/anthonykim02");
+// }
+
+
+
+
+
